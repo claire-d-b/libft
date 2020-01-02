85639da8b6db4974fcd8d0697ef55df2cb8111f5
Claire DE BERNY <clde-ber@e1r10p6.42.fr>
2020-01-02 15:31:09 +0100

    putstrfdputendlfd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,14 +6,12 @@
 /*   By: clde-ber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 14:11:42 by clde-ber          #+#    #+#             */
-/*   Updated: 2020/01/01 12:41:02 by clde-ber         ###   ########.fr       */
+/*   Updated: 2020/01/02 15:30:46 by clde-ber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (!s)
-		return ;
 	ft_putstr_fd(s, fd);
-	write(fd, "\n", 1);
+	ft_putchar_fd('\n', fd);
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,13 +6,21 @@
 /*   By: clde-ber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 12:19:42 by clde-ber          #+#    #+#             */
-/*   Updated: 2020/01/01 19:18:26 by clde-ber         ###   ########.fr       */
+/*   Updated: 2020/01/02 15:29:15 by clde-ber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	if (!s)
-		return ;
-	write(fd, s, ft_strlen(s));
+	size_t i;
+
+	i = 0;
+	if (s != NULL)
+	{
+		while (s[i])
+		{
+			ft_putchar_fd(s[i], fd);
+			i++;
+		}
+	}
 }
